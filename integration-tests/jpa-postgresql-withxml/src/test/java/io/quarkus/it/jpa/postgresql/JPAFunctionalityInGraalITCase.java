ae45b7c39a046ef4341e670dc1d261fbd8f38ef8
marko-bekhta <marko.prykladna@gmail.com>
2023-02-17 12:01:39 +0100

    Remove the check for `PostgresUUIDType` as there's no such class anymore

--- integration-tests/jpa-postgresql-withxml/src/test/java/io/quarkus/it/jpa/postgresql/JPAFunctionalityInGraalITCase.java
+++ integration-tests/jpa-postgresql-withxml/src/test/java/io/quarkus/it/jpa/postgresql/JPAFunctionalityInGraalITCase.java
@@ -18,7 +18,6 @@ public class JPAFunctionalityInGraalITCase extends JPAFunctionalityTest {
         //if not, that would be a sign that this test has become too weak
         //to identify the well working of the exclusions.
         report.assertContains(org.postgresql.jdbc.PgSQLXML.class);
-        report.assertContains(org.hibernate.type.PostgresUUIDType.class);
 
         //And finally verify we included the JDK XML by triggering
         //io.quarkus.jdbc.postgresql.runtime.graal.SQLXLMFeature
