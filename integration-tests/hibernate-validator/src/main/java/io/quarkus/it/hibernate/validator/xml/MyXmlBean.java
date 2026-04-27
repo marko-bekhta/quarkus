f384c730f15a41afdd05b0d4025184cb593f93f9
marko-bekhta <marko.prykladna@gmail.com>
2026-04-27 23:25:16 +0200

    WIP

--- integration-tests/hibernate-validator/src/main/java/io/quarkus/it/hibernate/validator/xml/MyXmlBean.java
+++ integration-tests/hibernate-validator/src/main/java/io/quarkus/it/hibernate/validator/xml/MyXmlBean.java
@@ -1,6 +1,10 @@
 package io.quarkus.it.hibernate.validator.xml;
 
+import io.quarkus.hibernate.accessor.runtime.ReflectionFreeAccessor;
+
 public class MyXmlBean {
+    @ReflectionFreeAccessor
     int id = 0;
+    @ReflectionFreeAccessor
     String name;
 }
