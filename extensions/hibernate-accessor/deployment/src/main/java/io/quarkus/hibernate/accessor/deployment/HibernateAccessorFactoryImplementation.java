bf3579391edf93be3d4e703d77048fe1b19e6807
marko-bekhta <marko.prykladna@gmail.com>
2026-04-30 19:00:48 +0200

    Switch to switch per class per member and single reader/writer

--- extensions/hibernate-accessor/deployment/src/main/java/io/quarkus/hibernate/accessor/deployment/HibernateAccessorFactoryImplementation.java
+++ extensions/hibernate-accessor/deployment/src/main/java/io/quarkus/hibernate/accessor/deployment/HibernateAccessorFactoryImplementation.java
@@ -1,7 +1,5 @@
 package io.quarkus.hibernate.accessor.deployment;
 
-import static io.quarkus.hibernate.accessor.deployment.HibernateAccessorPackageFactoryImplementation.PACKAGE_LEVEL_HIBERNATE_ACCESSOR_FACTORY;
-
 import java.lang.constant.ClassDesc;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -31,22 +29,72 @@ class HibernateAccessorFactoryImplementation {
 
     static final String QUARKUS_HIBERNATE_ACCESSOR_FACTORY = "io.quarkus.hibernate.accessor.runtime.QuarkusHibernateAccessorFactory";
 
-    private final List<String> packages = new ArrayList<>();
+    private static final String KEY_FORMAT = "%s.%s.%s";
+    private static final String TYPE_FIELD = "field";
+    private static final String TYPE_METHOD = "method";
+    private static final String TYPE_CONSTRUCTOR = "constructor";
 
-    void addPackage(String packageName) {
-        packages.add(packageName);
+    private final List<IndexEntry> readerEntries = new ArrayList<>();
+    private final List<IndexEntry> writerEntries = new ArrayList<>();
+    private final List<IndexEntry> instantiatorEntries = new ArrayList<>();
+
+    void addReaderEntry(String declaringClass, String type, String name, int classIndex, int memberIndex) {
+        readerEntries.add(new IndexEntry(
+                KEY_FORMAT.formatted(declaringClass, type, name), classIndex, memberIndex));
+    }
+
+    void addWriterEntry(String declaringClass, String type, String name, int classIndex, int memberIndex) {
+        writerEntries.add(new IndexEntry(
+                KEY_FORMAT.formatted(declaringClass, type, name), classIndex, memberIndex));
+    }
+
+    void addInstantiatorEntry(String declaringClass, String descriptor, int classIndex, int ctorIndex) {
+        instantiatorEntries.add(new IndexEntry(
+                KEY_FORMAT.formatted(declaringClass, TYPE_CONSTRUCTOR, descriptor), classIndex, ctorIndex));
+    }
+
+    void addFieldReader(String declaringClass, String fieldName, int classIndex, int memberIndex) {
+        addReaderEntry(declaringClass, TYPE_FIELD, fieldName, classIndex, memberIndex);
+    }
+
+    void addFieldWriter(String declaringClass, String fieldName, int classIndex, int memberIndex) {
+        addWriterEntry(declaringClass, TYPE_FIELD, fieldName, classIndex, memberIndex);
+    }
+
+    void addMethodReader(String declaringClass, String methodName, int classIndex, int memberIndex) {
+        addReaderEntry(declaringClass, TYPE_METHOD, methodName, classIndex, memberIndex);
+    }
+
+    void addMethodWriter(String declaringClass, String methodName, int classIndex, int memberIndex) {
+        addWriterEntry(declaringClass, TYPE_METHOD, methodName, classIndex, memberIndex);
     }
 
     void create(io.quarkus.gizmo2.Gizmo classGizmo) {
+        ClassDesc readerImplClass = ClassDesc.of(HibernateAccessorSingleImplGenerator.READER_IMPL);
+        ClassDesc writerImplClass = ClassDesc.of(HibernateAccessorSingleImplGenerator.WRITER_IMPL);
+        ClassDesc instantiatorImplClass = ClassDesc.of(HibernateAccessorSingleImplGenerator.INSTANTIATOR_IMPL);
+
         classGizmo.class_(QUARKUS_HIBERNATE_ACCESSOR_FACTORY, cc -> {
             cc.implements_(HibernateAccessorFactory.class);
             cc.public_();
 
-            FieldDesc delegates = cc.field("delegates", fc -> {
+            FieldDesc readers = cc.field("readers", fc -> {
                 fc.private_();
                 fc.final_();
                 fc.setType(GenericType.of(Map.class,
-                        List.of(TypeArgument.of(String.class), TypeArgument.of(HibernateAccessorFactory.class))));
+                        List.of(TypeArgument.of(String.class), TypeArgument.of(HibernateAccessorValueReader.class))));
+            });
+            FieldDesc writers = cc.field("writers", fc -> {
+                fc.private_();
+                fc.final_();
+                fc.setType(GenericType.of(Map.class,
+                        List.of(TypeArgument.of(String.class), TypeArgument.of(HibernateAccessorValueWriter.class))));
+            });
+            FieldDesc instantiators = cc.field("instantiators", fc -> {
+                fc.private_();
+                fc.final_();
+                fc.setType(GenericType.of(Map.class,
+                        List.of(TypeArgument.of(String.class), TypeArgument.of(HibernateAccessorInstantiator.class))));
             });
 
             final MethodDesc mapPut;
@@ -63,18 +111,34 @@ class HibernateAccessorFactoryImplementation {
                 conc.public_();
                 conc.body(bc -> {
                     bc.invokeSpecial(ConstructorDesc.of(cc.superClass()), conc.this_());
-                    bc.set(this_.field(delegates), bc.new_(GenericType.of(HashMap.class,
-                            List.of(TypeArgument.of(String.class), TypeArgument.of(HibernateAccessorFactory.class)))));
 
-                    for (String pkg : packages) {
-                        String internalFactoryName = pkg + "." + PACKAGE_LEVEL_HIBERNATE_ACCESSOR_FACTORY;
-                        ClassDesc factoryClass = ClassDesc.of(internalFactoryName);
-                        bc.invokeInterface(
-                                mapPut,
-                                this_.field(delegates),
-                                Const.of(pkg),
-                                bc.getStaticField(FieldDesc.of(factoryClass, "INSTANCE", factoryClass)));
+                    bc.set(this_.field(readers), bc.new_(GenericType.of(HashMap.class,
+                            List.of(TypeArgument.of(String.class),
+                                    TypeArgument.of(HibernateAccessorValueReader.class)))));
+                    bc.set(this_.field(writers), bc.new_(GenericType.of(HashMap.class,
+                            List.of(TypeArgument.of(String.class),
+                                    TypeArgument.of(HibernateAccessorValueWriter.class)))));
+                    bc.set(this_.field(instantiators), bc.new_(GenericType.of(HashMap.class,
+                            List.of(TypeArgument.of(String.class),
+                                    TypeArgument.of(HibernateAccessorInstantiator.class)))));
+
+                    for (IndexEntry entry : readerEntries) {
+                        bc.invokeInterface(mapPut, this_.field(readers),
+                                Const.of(entry.key()),
+                                bc.new_(readerImplClass, Const.of(entry.classIndex()), Const.of(entry.memberIndex())));
                     }
+                    for (IndexEntry entry : writerEntries) {
+                        bc.invokeInterface(mapPut, this_.field(writers),
+                                Const.of(entry.key()),
+                                bc.new_(writerImplClass, Const.of(entry.classIndex()), Const.of(entry.memberIndex())));
+                    }
+                    for (IndexEntry entry : instantiatorEntries) {
+                        bc.invokeInterface(mapPut, this_.field(instantiators),
+                                Const.of(entry.key()),
+                                bc.new_(instantiatorImplClass, Const.of(entry.classIndex()),
+                                        Const.of(entry.memberIndex())));
+                    }
+
                     bc.return_();
                 });
             });
@@ -84,12 +148,24 @@ class HibernateAccessorFactoryImplementation {
                 mc.returning(HibernateAccessorInstantiator.class);
                 ParamVar constructor = mc.parameter("constructor", Constructor.class);
                 mc.body(bc -> {
-                    Expr delegate = lookupDelegate(bc, this_, delegates, mapGet, constructor,
-                            MethodDesc.of(Constructor.class, "getDeclaringClass", Class.class));
-                    bc.return_(bc.invokeInterface(
-                            MethodDesc.of(HibernateAccessorFactory.class, "instantiator", HibernateAccessorInstantiator.class,
-                                    Constructor.class),
-                            delegate, constructor));
+                    Expr declClass = bc.invokeVirtual(
+                            MethodDesc.of(Constructor.class, "getDeclaringClass", Class.class), constructor);
+                    Expr className = bc.invokeVirtual(MethodDesc.of(Class.class, "getName", String.class), declClass);
+                    Expr descriptor = bc.invokeStatic(
+                            MethodDesc.of(
+                                    io.quarkus.hibernate.accessor.runtime.spi.NamingUtil.class,
+                                    "constructorDescriptor", String.class, Constructor.class),
+                            constructor);
+                    Expr key = bc.invokeVirtual(
+                            MethodDesc.of(String.class, "formatted", String.class, Object[].class),
+                            Const.of(KEY_FORMAT),
+                            bc.newArray(Object.class, className, Const.of(TYPE_CONSTRUCTOR), descriptor));
+                    LocalVar inst = bc.localVar("inst", HibernateAccessorInstantiator.class,
+                            bc.invokeInterface(mapGet, this_.field(instantiators), key));
+                    bc.ifElse(
+                            bc.isNull(inst),
+                            b1 -> b1.throw_(UnsupportedOperationException.class),
+                            b1 -> b1.return_(inst));
                 });
             });
 
@@ -98,12 +174,20 @@ class HibernateAccessorFactoryImplementation {
                 mc.returning(HibernateAccessorValueReader.class);
                 ParamVar field = mc.parameter("field", Field.class);
                 mc.body(bc -> {
-                    Expr delegate = lookupDelegate(bc, this_, delegates, mapGet, field,
-                            MethodDesc.of(Field.class, "getDeclaringClass", Class.class));
-                    bc.return_(bc.invokeInterface(
-                            MethodDesc.of(HibernateAccessorFactory.class, "valueReader", HibernateAccessorValueReader.class,
-                                    Field.class),
-                            delegate, field));
+                    Expr declClass = bc.invokeVirtual(
+                            MethodDesc.of(Field.class, "getDeclaringClass", Class.class), field);
+                    Expr className = bc.invokeVirtual(MethodDesc.of(Class.class, "getName", String.class), declClass);
+                    Expr memberName = bc.invokeVirtual(MethodDesc.of(Field.class, "getName", String.class), field);
+                    Expr key = bc.invokeVirtual(
+                            MethodDesc.of(String.class, "formatted", String.class, Object[].class),
+                            Const.of(KEY_FORMAT),
+                            bc.newArray(Object.class, className, Const.of(TYPE_FIELD), memberName));
+                    LocalVar reader = bc.localVar("reader", HibernateAccessorValueReader.class,
+                            bc.invokeInterface(mapGet, this_.field(readers), key));
+                    bc.ifElse(
+                            bc.isNull(reader),
+                            b1 -> b1.throw_(UnsupportedOperationException.class),
+                            b1 -> b1.return_(reader));
                 });
             });
 
@@ -112,12 +196,20 @@ class HibernateAccessorFactoryImplementation {
                 mc.returning(HibernateAccessorValueReader.class);
                 ParamVar method = mc.parameter("method", Method.class);
                 mc.body(bc -> {
-                    Expr delegate = lookupDelegate(bc, this_, delegates, mapGet, method,
-                            MethodDesc.of(Method.class, "getDeclaringClass", Class.class));
-                    bc.return_(bc.invokeInterface(
-                            MethodDesc.of(HibernateAccessorFactory.class, "valueReader", HibernateAccessorValueReader.class,
-                                    Method.class),
-                            delegate, method));
+                    Expr declClass = bc.invokeVirtual(
+                            MethodDesc.of(Method.class, "getDeclaringClass", Class.class), method);
+                    Expr className = bc.invokeVirtual(MethodDesc.of(Class.class, "getName", String.class), declClass);
+                    Expr memberName = bc.invokeVirtual(MethodDesc.of(Method.class, "getName", String.class), method);
+                    Expr key = bc.invokeVirtual(
+                            MethodDesc.of(String.class, "formatted", String.class, Object[].class),
+                            Const.of(KEY_FORMAT),
+                            bc.newArray(Object.class, className, Const.of(TYPE_METHOD), memberName));
+                    LocalVar reader = bc.localVar("reader", HibernateAccessorValueReader.class,
+                            bc.invokeInterface(mapGet, this_.field(readers), key));
+                    bc.ifElse(
+                            bc.isNull(reader),
+                            b1 -> b1.throw_(UnsupportedOperationException.class),
+                            b1 -> b1.return_(reader));
                 });
             });
 
@@ -126,12 +218,20 @@ class HibernateAccessorFactoryImplementation {
                 mc.returning(HibernateAccessorValueWriter.class);
                 ParamVar field = mc.parameter("field", Field.class);
                 mc.body(bc -> {
-                    Expr delegate = lookupDelegate(bc, this_, delegates, mapGet, field,
-                            MethodDesc.of(Field.class, "getDeclaringClass", Class.class));
-                    bc.return_(bc.invokeInterface(
-                            MethodDesc.of(HibernateAccessorFactory.class, "valueWriter", HibernateAccessorValueWriter.class,
-                                    Field.class),
-                            delegate, field));
+                    Expr declClass = bc.invokeVirtual(
+                            MethodDesc.of(Field.class, "getDeclaringClass", Class.class), field);
+                    Expr className = bc.invokeVirtual(MethodDesc.of(Class.class, "getName", String.class), declClass);
+                    Expr memberName = bc.invokeVirtual(MethodDesc.of(Field.class, "getName", String.class), field);
+                    Expr key = bc.invokeVirtual(
+                            MethodDesc.of(String.class, "formatted", String.class, Object[].class),
+                            Const.of(KEY_FORMAT),
+                            bc.newArray(Object.class, className, Const.of(TYPE_FIELD), memberName));
+                    LocalVar writer = bc.localVar("writer", HibernateAccessorValueWriter.class,
+                            bc.invokeInterface(mapGet, this_.field(writers), key));
+                    bc.ifElse(
+                            bc.isNull(writer),
+                            b1 -> b1.throw_(UnsupportedOperationException.class),
+                            b1 -> b1.return_(writer));
                 });
             });
 
@@ -140,28 +240,25 @@ class HibernateAccessorFactoryImplementation {
                 mc.returning(HibernateAccessorValueWriter.class);
                 ParamVar method = mc.parameter("method", Method.class);
                 mc.body(bc -> {
-                    Expr delegate = lookupDelegate(bc, this_, delegates, mapGet, method,
-                            MethodDesc.of(Method.class, "getDeclaringClass", Class.class));
-                    bc.return_(bc.invokeInterface(
-                            MethodDesc.of(HibernateAccessorFactory.class, "valueWriter", HibernateAccessorValueWriter.class,
-                                    Method.class),
-                            delegate, method));
+                    Expr declClass = bc.invokeVirtual(
+                            MethodDesc.of(Method.class, "getDeclaringClass", Class.class), method);
+                    Expr className = bc.invokeVirtual(MethodDesc.of(Class.class, "getName", String.class), declClass);
+                    Expr memberName = bc.invokeVirtual(MethodDesc.of(Method.class, "getName", String.class), method);
+                    Expr key = bc.invokeVirtual(
+                            MethodDesc.of(String.class, "formatted", String.class, Object[].class),
+                            Const.of(KEY_FORMAT),
+                            bc.newArray(Object.class, className, Const.of(TYPE_METHOD), memberName));
+                    LocalVar writer = bc.localVar("writer", HibernateAccessorValueWriter.class,
+                            bc.invokeInterface(mapGet, this_.field(writers), key));
+                    bc.ifElse(
+                            bc.isNull(writer),
+                            b1 -> b1.throw_(UnsupportedOperationException.class),
+                            b1 -> b1.return_(writer));
                 });
             });
         });
     }
 
-    private static Expr lookupDelegate(io.quarkus.gizmo2.creator.BlockCreator bc, This this_, FieldDesc delegates,
-            MethodDesc mapGet, ParamVar member, MethodDesc getDeclaringClass) {
-        Expr declClass = bc.invokeVirtual(getDeclaringClass, member);
-        Expr packageName = bc.invokeVirtual(MethodDesc.of(Class.class, "getPackageName", String.class), declClass);
-
-        LocalVar delegate = bc.localVar(
-                "delegate",
-                HibernateAccessorFactory.class,
-                bc.invokeInterface(mapGet, this_.field(delegates), packageName));
-
-        bc.if_(bc.isNull(delegate), b1 -> b1.throw_(IllegalStateException.class));
-        return delegate;
+    record IndexEntry(String key, int classIndex, int memberIndex) {
     }
 }
