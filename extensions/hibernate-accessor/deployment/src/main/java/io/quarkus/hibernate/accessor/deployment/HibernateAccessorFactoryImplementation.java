f381765c5640203a833c95e4888f75f60eb9fb00
Andrea Boriero <andrea@hibernate.org>
2026-05-11 16:36:06 +0200

    Make QuarkusHibernateAccessorFactory serializable

--- extensions/hibernate-accessor/deployment/src/main/java/io/quarkus/hibernate/accessor/deployment/HibernateAccessorFactoryImplementation.java
+++ extensions/hibernate-accessor/deployment/src/main/java/io/quarkus/hibernate/accessor/deployment/HibernateAccessorFactoryImplementation.java
@@ -1,10 +1,17 @@
 package io.quarkus.hibernate.accessor.deployment;
 
+import org.hibernate.accessor.HibernateAccessorFactory;
+
 import static io.quarkus.hibernate.accessor.deployment.HibernateAccessorGenerationUtil.STRING_SWITCH_CHUNK_SIZE;
 import static io.quarkus.hibernate.accessor.deployment.HibernateAccessorGenerationUtil.emitStringSwitch;
 import static io.quarkus.hibernate.accessor.deployment.HibernateAccessorGenerationUtil.fqcnToName;
 import static io.quarkus.hibernate.accessor.deployment.HibernateAccessorGenerationUtil.pushIntConst;
+import static io.quarkus.hibernate.accessor.deployment.HibernateAccessorSingleImplGenerator.INSTANTIATOR_INTERFACE;
+import static io.quarkus.hibernate.accessor.deployment.HibernateAccessorSingleImplGenerator.READER_INTERFACE;
+import static io.quarkus.hibernate.accessor.deployment.HibernateAccessorSingleImplGenerator.SERIALIZABLE_INTERFACE;
+import static io.quarkus.hibernate.accessor.deployment.HibernateAccessorSingleImplGenerator.WRITER_INTERFACE;
 
+import io.quarkus.hibernate.accessor.runtime.spi.NamingUtil;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
@@ -30,12 +37,9 @@ class HibernateAccessorFactoryImplementation implements Opcodes {
 
     private static final String FACTORY_INTERNAL = fqcnToName(QUARKUS_HIBERNATE_ACCESSOR_FACTORY);
 
-    private static final String READER_INTERFACE = "org/hibernate/accessor/HibernateAccessorValueReader";
-    private static final String WRITER_INTERFACE = "org/hibernate/accessor/HibernateAccessorValueWriter";
-    private static final String INSTANTIATOR_INTERFACE = "org/hibernate/accessor/HibernateAccessorInstantiator";
-    private static final String FACTORY_INTERFACE = "org/hibernate/accessor/HibernateAccessorFactory";
+    private static final String FACTORY_INTERFACE = fqcnToName( HibernateAccessorFactory.class.getName());
 
-    private static final String NAMING_UTIL = "io/quarkus/hibernate/accessor/runtime/spi/NamingUtil";
+    private static final String NAMING_UTIL = fqcnToName( NamingUtil.class.getName());
 
     private final Map<String, String> dispatchTargets = new LinkedHashMap<>();
     private final Set<String> interfaceTargets = new HashSet<>();
@@ -77,13 +81,18 @@ class HibernateAccessorFactoryImplementation implements Opcodes {
         ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
 
         cw.visit(V17, ACC_PUBLIC | ACC_SUPER, FACTORY_INTERNAL, null,
-                "java/lang/Object", new String[] { FACTORY_INTERFACE });
+                "java/lang/Object", new String[] { FACTORY_INTERFACE, SERIALIZABLE_INTERFACE });
 
         if (withFallback) {
             cw.visitField(ACC_PRIVATE | ACC_FINAL, "fallback", "L" + FACTORY_INTERFACE + ";", null, null).visitEnd();
         }
 
+        // Static singleton reference for readResolve() — preserves singleton across deserialization.
+        cw.visitField(ACC_PRIVATE | ACC_STATIC, "INSTANCE", "L" + FACTORY_INTERNAL + ";", null, null).visitEnd();
+
         generateConstructor(cw, withFallback);
+
+        generateReadResolve(cw);
 
         generateValueAccessor(cw, withFallback, "valueReader", "java/lang/reflect/Field", "getName",
                 FIELD_READER, READER_INTERFACE, fieldReaderClasses);
@@ -117,10 +126,24 @@ class HibernateAccessorFactoryImplementation implements Opcodes {
             mv.visitCode();
             mv.visitVarInsn(ALOAD, 0);
             mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+            // Store the singleton reference for readResolve()
+            mv.visitVarInsn(ALOAD, 0);
+            mv.visitFieldInsn(PUTSTATIC, FACTORY_INTERNAL, "INSTANCE", "L" + FACTORY_INTERNAL + ";");
             mv.visitInsn(RETURN);
             mv.visitMaxs(0, 0);
             mv.visitEnd();
         }
+    }
+
+    // readResolve() returns the static INSTANCE so deserialization preserves the singleton.
+    private static void generateReadResolve(ClassWriter cw) {
+        MethodVisitor mv = cw.visitMethod(ACC_PRIVATE, "readResolve",
+                                          "()Ljava/lang/Object;", null, null);
+        mv.visitCode();
+        mv.visitFieldInsn(GETSTATIC, FACTORY_INTERNAL, "INSTANCE", "L" + FACTORY_INTERNAL + ";");
+        mv.visitInsn(ARETURN);
+        mv.visitMaxs(0, 0);
+        mv.visitEnd();
     }
 
     private void generateValueAccessor(ClassWriter cw, boolean withFallback, String factoryMethodName,
--- extensions/hibernate-accessor/deployment/src/main/java/io/quarkus/hibernate/accessor/deployment/HibernateAccessorSingleImplGenerator.java
+++ extensions/hibernate-accessor/deployment/src/main/java/io/quarkus/hibernate/accessor/deployment/HibernateAccessorSingleImplGenerator.java
@@ -8,6 +8,7 @@ import static io.quarkus.hibernate.accessor.deployment.HibernateAccessorHostClas
 import static io.quarkus.hibernate.accessor.deployment.HibernateAccessorHostClassFunction.WRITE_METHOD;
 import static io.quarkus.hibernate.accessor.deployment.HibernateAccessorProcessor.*;
 
+import java.io.Serializable;
 import java.util.List;
 
 import org.hibernate.accessor.HibernateAccessorInstantiator;
@@ -24,9 +25,11 @@ class HibernateAccessorSingleImplGenerator implements Opcodes {
     static final String WRITER_IMPL = "io.quarkus.hibernate.accessor.runtime.QuarkusHibernateAccessorValueWriterImpl";
     static final String INSTANTIATOR_IMPL = "io.quarkus.hibernate.accessor.runtime.QuarkusHibernateAccessorInstantiatorImpl";
 
-    private static final String READER_INTERFACE = fqcnToName(HibernateAccessorValueReader.class.getName());
-    private static final String WRITER_INTERFACE = fqcnToName(HibernateAccessorValueWriter.class.getName());
-    private static final String INSTANTIATOR_INTERFACE = fqcnToName(HibernateAccessorInstantiator.class.getName());
+    static final String READER_INTERFACE = fqcnToName(HibernateAccessorValueReader.class.getName());
+    static final String WRITER_INTERFACE = fqcnToName(HibernateAccessorValueWriter.class.getName());
+    static final String INSTANTIATOR_INTERFACE = fqcnToName(HibernateAccessorInstantiator.class.getName());
+
+    static final String SERIALIZABLE_INTERFACE = fqcnToName( Serializable.class.getName());
 
     byte[] generateReaderImpl(List<HostData> hostClasses) {
         String className = fqcnToName(READER_IMPL);
@@ -34,7 +37,7 @@ class HibernateAccessorSingleImplGenerator implements Opcodes {
 
         cw.visit(V17, ACC_PUBLIC | ACC_SUPER, className,
                 "Ljava/lang/Object;L" + READER_INTERFACE + "<Ljava/lang/Object;>;",
-                "java/lang/Object", new String[] { READER_INTERFACE });
+                "java/lang/Object", new String[] { READER_INTERFACE, SERIALIZABLE_INTERFACE });
 
         generateIndexFields(cw);
         generateIndexConstructor(cw, className);
@@ -61,7 +64,7 @@ class HibernateAccessorSingleImplGenerator implements Opcodes {
 
         cw.visit(V17, ACC_PUBLIC | ACC_SUPER, className,
                 null,
-                "java/lang/Object", new String[] { WRITER_INTERFACE });
+                "java/lang/Object", new String[] { WRITER_INTERFACE, SERIALIZABLE_INTERFACE });
 
         generateIndexFields(cw);
         generateIndexConstructor(cw, className);
@@ -87,7 +90,7 @@ class HibernateAccessorSingleImplGenerator implements Opcodes {
 
         cw.visit(V17, ACC_PUBLIC | ACC_SUPER, className,
                 "Ljava/lang/Object;L" + INSTANTIATOR_INTERFACE + "<Ljava/lang/Object;>;",
-                "java/lang/Object", new String[] { INSTANTIATOR_INTERFACE });
+                "java/lang/Object", new String[] { INSTANTIATOR_INTERFACE, SERIALIZABLE_INTERFACE });
 
         generateIndexFields(cw);
         generateIndexConstructor(cw, className);
